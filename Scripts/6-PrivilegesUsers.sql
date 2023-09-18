7366d3fb66adf1217efe58618ec9e708792477a7
Raphael Nicaise <raphanicaise@gmail.com>
2023-09-18 19:23:10 -0300

    Trigger de usuarios / Privileges

--- Scripts/6-PrivilegesUsers.sql
+++ Scripts/6-PrivilegesUsers.sql
@@ -1,16 +1,19 @@
--- Crea al USUARIO1 en el localhost, y le asigna su contraseña
-CREATE USER USUARIO1@localhost IDENTIFIED BY '123456789';
--- Le da a USUARIO1 solo la posibilidad de Leer (Hacer Selects a todas tablas y vistas)
-GRANT SELECT ON remerasrapha2.* TO USUARIO1@localhost;
--- Crea al USUARIO2 en el localhost, y le asigna su contraseña
-CREATE USER USUARIO2@localhost IDENTIFIED BY 'qwertyuiop';
--- Le da al USUARIO2 la posibilidad de hacer Selects a todas las tablas y vistas
--- Tambien tiene la posibilidad de hacer Inserts y Updates a cualquier tabla
-GRANT SELECT,INSERT,UPDATE ON remerasrapha2.* TO USUARIO2@localhost;
+-- Crea al usuario administracion en el localhost, y le asigna su contraseña
+CREATE USER administracion@localhost IDENTIFIED BY '123456789';
+-- Le da a USUARIO administracion la posibilidad de hacer selects, inserts, y update a todas las tablas
+GRANT SELECT,INSERT,UPDATE ON remerasrapha2.* TO administracion@localhost;
+
+-- Crea al usuario fabrica_remeras en el localhost, y le asigna su contraseña
+CREATE USER fabrica_remeras@localhost IDENTIFIED BY 'qwertyuiop';
+
+-- Le da al usuario fabricas_remeras la posibilidad de hacer Selects vistas que le sirven para fabricar las remeras
+
+GRANT SELECT ON colores_remeras TO fabrica_remeras@localhost;
+GRANT SELECT ON diseños_remeras TO fabrica_remeras@localhost; 
 
 -- Muestran los privilegios concedidos
-SHOW GRANTS FOR USUARIO1@localhost;
-SHOW GRANTS FOR USUARIO2@localhost;
+SHOW GRANTS FOR administracion@localhost;
+SHOW GRANTS FOR fabrica_remeras@localhost;
 
 
 /* revoke select on *.* from USUARIO1@localhost;
@@ -18,5 +21,4 @@ revoke select,insert,update on *.* from USUARIO2@localhost; */
 
 -- Supuestamente esto actualiza los privilegios (?
 				FLUSH PRIVILEGES;
-
 
